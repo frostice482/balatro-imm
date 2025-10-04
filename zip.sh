172d19c257510a0cdc03c818a11b14e955e6c34c
frostice482 <frostice48260@gmail.com>
2025-10-04 07:41:10 +0700

    fix failed distribution

--- zip.sh
+++ zip.sh
@@ -1,2 +1,2 @@
 rm -f inmodman.zip
-zip -r inmodman.zip assets imm include lovely main.lua emergency_err.lua license manifest.json README.md
+zip -r inmodman.zip assets imm include lovely main.lua early_err.lua license manifest.json README.md
