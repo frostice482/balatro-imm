055bbe1e5ee2df9f4633c3921825ca65a254f08f
frostice482 <frostice48260@gmail.com>
2025-10-13 01:36:37 +0700

    don't throw when https not available

--- imm/https_thread.lua
+++ imm/https_thread.lua
@@ -1,15 +1,22 @@
 --- @type love.Channel
 local i = ...
-local https = require('https')
+local hok, https
 require('love.event')
+
+local function process(req)
+    if not https then
+        hok, https = pcall(require, 'https')
+        if not hok then
+            return { -1, "Failed to initialize https: " .. https }
+        end
+    end
+
+    local res = { pcall(https.request, req.url, req.options) }
+    return res[1] and {unpack(res, 2, 4)} or {-1, res[2]}
+end
 
 while true do
     local msg = i:demand()
-    local res = {pcall(https.request, msg.req.url, msg.req.options)}
-
-    love.event.push('imm_taskres', --- @diagnostic disable-line
-        msg.gid,
-        msg.id,
-        res[1] and {unpack(res, 2, 4)} or {-1, res[2]}
-    )
+    local res = process(msg.req)
+    love.event.push('imm_taskres', msg.gid, msg.id, res) --- @diagnostic disable-line
 end
