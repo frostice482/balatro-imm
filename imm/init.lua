f1371ab83b096beeacb8b5366e7cdd7002a799d6
frostice482 <frostice48260@gmail.com>
2025-12-22 21:20:51 +0700

    why this gone

--- imm/init.lua
+++ imm/init.lua
@@ -1,6 +1,8 @@
 local util = require("imm.lib.util")
 local ui = require("imm.lib.ui")
 
+require("imm.init_config")
+
 package.preload['imm.tasks'] = function () return require("imm.btasks.tasks")() end
 package.preload['imm.repo'] = function () return require("imm.modrepo.repo")() end
 package.preload['imm.modpacks'] = function ()
